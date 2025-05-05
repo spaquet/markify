1aa6f698ae0e29990d743107480079b7e6ec843d
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-05-05 13:10:03 -0700

    Initial Commit

---
+++ Markify/MarkifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkifyApp.swift
+//  Markify
+//
+//  Created by Stéphane PAQUET on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MarkifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
